065174f5c90ecfb8f72d4c316515fdc2e0e1bd4d
f95f <95felipefernandes@gmail.com>
2025-03-02 04:08:01 -0300

    feat: add lists and list details components with routing

---
+++ src/app/sections/list-details/list-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-details',
+  imports: [],
+  templateUrl: './list-details.component.html',
+  styleUrl: './list-details.component.css'
+})
+export class ListDetailsComponent {
+
+}
